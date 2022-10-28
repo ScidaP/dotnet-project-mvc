07490bdd02c9e0945477ee62bf17f9574be85c5a
Patricio Scida <patricioscida@gmail.com>
2022-10-28 18:27:04 -0300

    agregando views y controller de pedidos

---
+++ Controllers/PedidosController.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Tp4MvcNuevo.Models;
+
+namespace Tp4MvcNuevo.Controllers;
+
+public class PedidosController : Controller
+{
+    public IActionResult HacerPedido() {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult PedidoAgregado(int numero, string obs, string estado) {
+        return View();
+    }
+}
