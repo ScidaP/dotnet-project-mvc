d460cf16c4197ec98a67c8575d5fb2b468d88ab2
Patricio Scida <patricioscida@gmail.com>
2022-10-28 18:32:46 -0300

    agregados los modelos del tp3

---
+++ Models/Models.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tp4MvcNuevo.Models {
+
+    class Persona {
+        private int id;
+        private string nombre;
+        private string direccion;
+        private long telefono;
+
+        public Persona(int id, string nombre, string direccion, long telefono) {
+            Id = id;
+            Nombre = nombre;
+            Direccion = direccion;
+            Telefono = telefono;
+        }
+
+        public virtual void MostrarDatos() {
+            Console.WriteLine("ID: " + Id);
+            Console.WriteLine("Nombre: " + Nombre);
+            Console.WriteLine("Direccion: " + Direccion);
+            Console.WriteLine("Telefono: " + Telefono);
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public long Telefono { get => telefono; set => telefono = value; }
+    }
+
+    class Cliente : Persona {
+        private string referenciasDireccion;
+
+        public Cliente(int id, string nombre, string direccion, long telefono, string referenciasDireccion) : base(id, nombre, direccion, telefono) {
+            ReferenciasDireccion = referenciasDireccion;
+        } 
+
+        public string ReferenciasDireccion { get => referenciasDireccion; set => referenciasDireccion = value; }
+    }
+
+    class Cadete : Persona {
+        private List<Pedido> ListaPedidos;
+        private double TotalACobrar;
+
+        public Cadete(int id, string nombre, string direccion, long telefono, double totalACobrar1, List<Pedido> listaPedidos1) : base(id, nombre, direccion, telefono) {
+            TotalACobrar1 = totalACobrar1;
+            ListaPedidos1 = listaPedidos1;
+        }
+
+        public override void MostrarDatos() {
+            base.MostrarDatos(); 
+            Console.WriteLine("Total a Cobrar: " + TotalACobrar1);
+        }
+        public double TotalACobrar1 { get => TotalACobrar; set => TotalACobrar = value; }
+        internal List<Pedido> ListaPedidos1 { get => ListaPedidos; set => ListaPedidos = value; }
+    }
+
+        class Cadeteria {
+        private string nombre;
+        private long telefono;
+        private List<Cadete> ListaCadetes;
+
+        public Cadeteria(string nombre, long telefono, List<Cadete> listaCadetes1) {
+            Nombre = nombre;
+            Telefono = telefono;
+            ListaCadetes1 = listaCadetes1;
+        }
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public long Telefono { get => telefono; set => telefono = value; }
+        internal List<Cadete> ListaCadetes1 { get => ListaCadetes; set => ListaCadetes = value; }
+    }
+    class Pedido {
+        private int numero;
+        private string obs;
+        private Cliente datosCliente;
+        private string estado;
+
+        public Pedido(int numero, string obs, string estado, Cliente datosCliente) {
+            Numero = numero;
+            Obs = obs;
+            Estado = estado;
+            DatosCliente = datosCliente;
+        }
+
+        public void CambiarEstado(string EstadoNuevo) {
+            Estado = EstadoNuevo;
+        }
+        public int Numero { get => numero; set => numero = value; }
+        public string Obs { get => obs; set => obs = value; }
+        public string Estado { get => estado; set => estado = value; }
+        internal Cliente DatosCliente { get => datosCliente; set => datosCliente = value; }
+    }
+}
