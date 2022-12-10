2946bb0ba4dc9bfcdf3aad35e198430b5332ce97
Patricio Scidá <patricioscida@gmail.com>
2022-12-10 20:01:51 -0300

    terminado repositorio pedidos

--- Controllers/PedidosController.cs
+++ Controllers/PedidosController.cs
@@ -9,12 +9,24 @@ using System.Linq;
 using System.Globalization;
 using System.Text;
 using Tp4MvcNuevo.ViewModels;
+using AutoMapper;
 
 namespace Tp4MvcNuevo.Controllers;
 
 public class PedidosController : Controller
 {
+    private readonly IRepositorioCadetes repoCadetes;
+    private readonly IMapper mapper;
+
+    public PedidosController(IRepositorioCadetes repoCad, IMapper mapp) {
+        repoCadetes = repoCad;
+        mapper = mapp;
+    }
     public IActionResult HacerPedido() {
         return View();
+    }
+
+    public IActionResult PedidoAgregado(string obs, string estado, string numeroCliente, string nombreCadete) {
+        long numeroTelCliente = Convert.ToInt64(numeroCliente);
     }
 }
--- Repositorios/RepositorioPedidos.cs
+++ Repositorios/RepositorioPedidos.cs
@@ -6,9 +6,11 @@ using Tp4MvcNuevo.Models;
 public interface IRepositorioPedidos {
     public Pedido getPedido(int id);
     public void agregarPedido(Pedido ped);
-    //public void eliminarPedido(int id);
-    //public List<Pedido> getTodosPedidos();
-    //public void actualizarPedido(Pedido cad);
+    public void eliminarPedido(int id);
+    public List<Pedido> getTodosPedidos();
+    public void actualizarPedido(Pedido ped);
+    public int? getIdCliente(int telefono); //Te devuelve el id (o null) de un cliente al buscarlo por su numero de telefono
+    public int? getIdCadete(string nombre);
 }
 
 public class RepositorioPedidos : IRepositorioPedidos {
@@ -41,5 +43,84 @@ public class RepositorioPedidos : IRepositorioPedidos {
             command.ExecuteNonQuery();
             conexion.Close();
         }
+    }
+
+    public void eliminarPedido(int id) {
+        using (var conexion = new SQLiteConnection("Data Source=DB/basededatos.db")) {
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = @"DELETE FROM pedidos WHERE id=$id";
+            command.Parameters.AddWithValue("$id", id);
+            command.ExecuteNonQuery();
+            conexion.Close();
+        }
+    }
+
+    public List<Pedido> getTodosPedidos() {
+        List<Pedido> ListaPedidos = new List<Pedido>();
+        using (var conexion = new SQLiteConnection("Data Source=DB/basededatos.db")) {
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = @"SELECT * FROM pedidos";
+            using (var reader = command.ExecuteReader()) {
+                while (reader.Read()) {
+                    var pedido = new Pedido(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetString(3), reader.GetInt32(4));
+                    ListaPedidos.Add(pedido);
+                }
+            }
+            conexion.Close();
+        }
+        return ListaPedidos;
+    }
+
+    public void actualizarPedido(Pedido ped) {
+        using (var conexion = new SQLiteConnection("Data Source=DB/basededatos.db")) {
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = @"UPDATE pedidos SET obs = $obs, cliente = $cliente, estado = $estado, cadete = $cadete WHERE id = $id";
+            command.Parameters.AddWithValue("$obs", ped.Obs);
+            command.Parameters.AddWithValue("$cliente", ped.idCliente1);
+            command.Parameters.AddWithValue("$estado", ped.Estado);
+            command.Parameters.AddWithValue("$cadete", ped.idCadete1);
+            command.Parameters.AddWithValue("$id",  ped.Numero);
+            command.ExecuteNonQuery();
+            conexion.Close();
+        }
+    }
+
+    public int? getIdCliente(int telefono) {
+        int? idCliente = null;
+        using (var conexion = new SQLiteConnection("Data Source=DB/basededatos.db")) {
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = @"SELECT id FROM cliente WHERE telefono like $telefono";
+            command.Parameters.AddWithValue("$telefono", telefono);
+            using (var reader = command.ExecuteReader()) {
+                while (reader.Read()) {
+                    if (reader.GetInt32(0) >= 0) {
+                        idCliente = reader.GetInt32(0);
+                    }
+                }
+            }
+        }
+        return idCliente;
+    }
+
+    public int? getIdCadete(string nombre) {
+        int? idCadete = null;
+        using (var conexion = new SQLiteConnection("Data Source=DB/basededatos.db")) {
+            conexion.Open();
+            var command = conexion.CreateCommand();
+            command.CommandText = @"SELECT id FROM cadete WHERE nombre like $nombre";
+            command.Parameters.AddWithValue("$nombre", nombre);
+            using (var reader = command.ExecuteReader()) {
+                while (reader.Read()) {
+                    if (reader.GetInt32(0) >= 0) {
+                        idCadete = reader.GetInt32(0);
+                    }
+                }
+            }
+        }
+        return idCadete;
     }
 }
